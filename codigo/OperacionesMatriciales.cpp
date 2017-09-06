59debc9b21943822fa84038f0252d239cedfcf58
mam <martin.monti@sisa.com.ar>
2017-09-06 11:55:29 -0300

    Armado de Matriz M

--- codigo/OperacionesMatriciales.cpp
+++ codigo/OperacionesMatriciales.cpp
@@ -112,3 +112,48 @@ double* OperacionesMatriciales::resolverTriangularInferior(vector<vector<double>
 		return resultado;		
 }
 
+/** "cantFilasImagen" es la cantidad de filas de la Imagen.
+"cantColumnasImagen" es la cantidad de columnas de la Imagen.
+"normales" contiene la norma perpendicular a la superficie del objeto estudiado para cada píxel de
+la imagen. Las normales son tridimensionales.
+De las tres dimensiones de cada normal, la primera es n_x, la segunda, n_y y la tercera, n_z.
+Sea N = cantFilasImagen*cantColumnasImagen. N resulta la cantidad de píxeles de la imagen.
+El método genera la matriz M con las ecuaciones (11) y (12) del enunciado.
+M será de 2N filas y N+1 columnas (la última columna será el término independiente del sistema de
+ecuaciones.
+Precondición: normales es de dimensiones cantFilasImagenXcantColumnasImagenX3 (que es igual a Nx3).
+*/
+vector<vector<double> > OperacionesMatriciales::generarMatrizM(vector<vector<vector<double> > > normales,int cantFilasImagen, int cantColumnasImagen){
+    int N = cantFilasImagen*cantColumnasImagen;
+    vector<vector<double> > M = vector<vector<double> >(2*N);        // 2*N filas
+
+    // Inicializo M con todos ceros:
+    for(int filaM=0;filaM<2*N;filaM+=2){
+        M[filaM] = vector<double>(N+1,0);
+        M[filaM+1] = vector<double>(N+1);
+        for(int columnaM=0;columnaM<=N;columnaM++){M[filaM][columnaM]=0; M[filaM+1][columnaM]=0;}
+    }
+    // Agrego valores no nulos a M:
+    for(int x=0;x<cantFilasImagen;x++) for(int y=0;y<cantColumnasImagen;y++){
+        int colM_x_y = cantColumnasImagen*x+y;
+        int filaM_x_y = colM_x_y*2;
+
+        int colM_x_yMas1 = (y==cantColumnasImagen-1)?-1:colM_x_y+1; // -1 Significa "INDEFINIDO"
+        int colM_xMas1_y = (x==cantFilasImagen-1)?-1:colM_x_y+cantColumnasImagen;
+
+        double n_x = normales[x][y][0];
+        double n_y = normales[x][y][1];
+        double n_z = normales[x][y][2];
+
+        M[filaM_x_y][colM_x_y] = -n_z;                            // Ecuación (11) - "Diagonal"
+        if(colM_xMas1_y>-1) M[filaM_x_y][colM_xMas1_y] = n_z;    // Ecuación (11) - Otro píxel
+        M[filaM_x_y][N] = -n_x;                                // Ecuación (11) - Término independiente
+
+        M[filaM_x_y+1][colM_x_y] = -n_z;                        // Ecuación (12) - "Diagonal"
+        if(colM_x_yMas1>-1) M[filaM_x_y+1][colM_x_yMas1] = n_z;    // Ecuación (12) - Otro píxel
+        M[filaM_x_y+1][N] = -n_y;                            // Ecuación (12) - Término independiente
+    }
+
+    return M;
+}
+
--- codigo/OperacionesMatriciales.h
+++ codigo/OperacionesMatriciales.h
@@ -14,5 +14,6 @@ class OperacionesMatriciales {
 		static double* resolverTriangularSuperior(vector<vector<double> > matrix, int dim);
 		static double* resolverTriangularSuperior(vector<vector<double> >, int dim, double* resultado);
 		static double* resolverTriangularInferior(vector<vector<double> >, int dim);
+		static vector<vector<double> > generarMatrizM(vector<vector<vector<double> > > normales,int cantFilasImagen, int cantColumnasImagen);
 };
 #endif
