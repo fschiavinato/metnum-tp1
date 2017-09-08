86814468d7ab7a8db32fc85c1aff4d090bcc2121
fschiavinato <francocschiavinato@gmail.com>
2017-09-08 00:42:40 -0300

    Funciona el LU con pivoteo. Es mejor usar el LU en vez de EG para el calculo de las normales ya que se usa siempre la misma matriz.

--- codigo/lu.cpp
+++ codigo/lu.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
    
-#define DEBUG(a,n,m)  cout << #a << " = " << endl;\
+#define DEBUGM(a,n,m)  cout << #a << " = " << endl;\
     for(int i = 0; i < n; i++) { \
         for(int j = 0; j < m; j++) {\
             cout << a[i][j] << " ";\
@@ -11,20 +13,37 @@ using namespace std;
     } \
     cout << endl;\
 
+#define DEBUGV(a,n)  cout << #a << " = " << endl;\
+    for(int i = 0; i < n; i++) {\
+        cout << a[i] << " ";\
+    }\
+    cout << endl;\
 
-void LU(float** A, int n, int m) {
-    float **&L = A;
-    float **&U = A;
-
+void LU(vector<vector<double>> &A, int n, int m, vector<int> &p) {
+    vector<vector<double>> &L = A;
+    vector<vector<double>> &U = A;
+    for(int i = 0; i < n; i++) p[i] = i;
     for(int l = 0; l < n - 1; l++) {
-        if(U[l][l] != 0) {
-            for(int i = l + 1; i < n; i++) {
-                float alpha = U[i][l] / U[l][l];
-                L[i][l] = alpha;
+        double maximo = U[l][l];
+        for(int pivote = l; pivote < n; pivote++) {
+            if(maximo < abs(U[pivote][l])) {
+                p[l] = pivote;
+                maximo = abs(U[pivote][l]);
+            }
+        }
 
-                for(int j = l+1; j < m; j++) {
-                    U[i][j] = U[i][j] - alpha * U[l][j];
-                }
+        if(l != p[l]) {
+            swap(A[l], A[p[l]]);
+            p[p[l]] = l;
+        }
+
+        for(int i = l + 1; i < n; i++) {
+
+            double alpha = U[i][l] / U[l][l];
+            L[i][l] = alpha;
+
+            for(int j = l+1; j < m; j++) {
+                U[i][j] = U[i][j] - alpha * U[l][j];
             }
         }
     }
@@ -32,10 +51,11 @@ void LU(float** A, int n, int m) {
 
 int main() {
 
-    float** a = new float*[3];
+    vector<vector<double>> a(3);
     for(int i = 0; i < 3; i++) {
-        a[i] = new float[3];
+        a[i].resize(3);
     }
+    vector<int> p(3);
     a[0][0] = 7;
     a[0][1] = 3;
     a[0][2] = -11;
@@ -45,7 +65,8 @@ int main() {
     a[2][0] = -11;
     a[2][1] = 2;
     a[2][2] = -2;
-    LU(a, 3, 3);
-    DEBUG(a,3,3)
+    LU(a, 3, 3, p);
+    DEBUGM(a,3,3)
+    DEBUGV(p,3)
 
 }
